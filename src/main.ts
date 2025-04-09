541d8d7969be7b799c0f4ed839e015b2e86e3bac
t-takeyasu <aferandona@gmail.com>
2025-04-10 08:34:30 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
